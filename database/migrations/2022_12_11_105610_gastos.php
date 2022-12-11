d52be6030dac0e69898a61394d83c2aa3ec67eeb
Kevin Huayhuas <kevin.huayhuas@gmail.com>
2022-12-11 11:55:17 -0500

    migraiones para gastos mensuales

---
+++ database/migrations/2022_12_11_105610_gastos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagos', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre_pago");
+            $table->string("detalle_pago");
+            $table->string("entidad");
+            $table->string("codigo_pago");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/migrations/2022_12_11_112839_cronogramas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cronogramas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("gasto_id");
+            $table->date("fecha_vencimiento");
+            $table->date("fecha_pago");
+            $table->boolean("estado");
+            $table->timestamps();
+            $table->foreign('gasto_id')->references('id')->on('pagos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
